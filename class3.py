9f8af13020611ce24d0bcb2420d54d24bdf097de
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-01-19 10:43:36 +0100

    Using the initiaal function in the new function defined

--- class3.py
+++ class3.py
@@ -5,6 +5,13 @@ def maths(x, y):
     a = [[2, y, 4,],
          [x, 5, 6]]
     print(a[0][1] + a[1][2])
+    
+def cal():
+    x = random.randint(0, 8)
+    y = random.randint(0, 8)
+    print(x)
+    print(y)
+    maths(x, y)
 
 quit()
 x = 20
