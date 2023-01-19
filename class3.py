2c1b169b1f090c184b676f75798e4615cd7563bc
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-01-19 10:46:35 +0100

    New Update

--- class3.py
+++ class3.py
@@ -1,6 +1,23 @@
 import random
 
+################################################################
+number = random.randint(1, 10000000000)
+#print("The generated number is {}".format(number))
 
+num_list = [str(random.randint(0, 10)) for n in range(10)]
+number = ''.join(num_list)
+print(number)
+print("The generated ten-digit number is {}".format(number))
+
+def top_up(num):
+    num = int(input())
+    if num == int(number):
+        print("""Your recharge of N{} was successful
+        Your account balance is N{}""".format(300, 300+150))
+        print('j')
+
+
+############################################################
 def maths(x, y):
     a = [[2, y, 4,],
          [x, 5, 6]]
