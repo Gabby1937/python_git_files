c96e6e62d3a18d6b7fe9ba19c7764137bb55360e
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-01-19 10:34:18 +0100

    Created a link reciever, scan the link with urllib, parse html with beautiful soup

--- bs4_link_srapper.py
+++ bs4_link_srapper.py
@@ -1,3 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 
+url = input("Enter - ")
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
