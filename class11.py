2ddf05ecc48680ea079646c53e004836610882a3
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-01-23 23:24:51 +0100

    names and tuples

--- class11.py
+++ class11.py
@@ -9,3 +9,13 @@ print(list(zip(d, x)))
 oh = ('chals', 'the', 'hopper')
 x = list(oh)
 print(x)
+
+(DEFAULT, 'elisha', 9395),
+(DEFAULT, 'Desa', 1002),
+(DEFAULT, 'John', 1001),
+(DEFAULT, 'Danny', 1003),
+(DEFAULT, 'Josh', 4322),
+(DEFAULT, 'Donal', 9873),
+(DEFAULT, 'Presha', 4395),
+(DEFAULT, 'Clinton', 7059),
+(DEFAULT, 'Sammy', 6422)
