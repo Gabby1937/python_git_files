c4aa7506600fcb55a080b6545a4b0b08aa04e33e
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-01-24 23:55:12 +0100

    While loop

--- class21.py
+++ class21.py
@@ -5,3 +5,14 @@ import time
 #range(start, stop, [step])
 #range(1, 50, 2)
 #range is a function used to select two ends in a code
+
+
+um = 1
+
+while num > 0:
+    print("getting number ", num)
+    #time.sleep(1)
+    num = num + 100
+    if num == 1000000000000:
+        break
+    
