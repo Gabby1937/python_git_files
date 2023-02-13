f6b3bc91d41991aa75a89135035935d7100823a4
Gabriel .C. Johnson <gabrieljohnson1937@gmail.com>
2023-02-13 14:14:53 +0100

    She listed out the menu for the game.

--- RumeRPS.py
+++ RumeRPS.py
@@ -1,3 +1,11 @@
 import random
 
 words = ["rock", "paper","scissor"]
+
+def grace():
+      print("""
+            select any of the options:
+            R for rock
+            P for paper
+            S for scissors
+            """)
